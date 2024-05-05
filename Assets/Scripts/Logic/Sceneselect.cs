9293b312d04fc6dbe69bd56fed663ade4f13ccaa
TMF133 <tmf23@mails.tsinghua.edu.cn>
2024-05-05 16:17:18 +0800

    Update 0505 fin.

--- Assets/Scripts/Logic/Sceneselect.cs
+++ Assets/Scripts/Logic/Sceneselect.cs
@@ -9,6 +9,7 @@ public class Sceneselect : MonoBehaviour
     Vector2 mousepos;
     Vector2 defaultpos = new Vector2(0, 0);
     public int num = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,11 +25,17 @@ public class Sceneselect : MonoBehaviour
             ani.SetBool("Selected", true);
             if (Input.GetMouseButton(0))
             {
-                SceneSwitchScript.instance.sceneNum=num;
-                LogicScript.instance.startPos = defaultpos;
-                SceneManager.LoadScene(SceneSwitchScript.instance.scenes[num]);
-                SceneSwitchScript.instance.aud.clip = SceneSwitchScript.instance.clips[num - 2];
-                SceneSwitchScript.instance.aud.Play();
+                
+              
+                
+                
+                    SceneSwitchScript.instance.sceneNum = num;
+                    LogicScript.instance.startPos = defaultpos;
+                    SceneManager.LoadScene(SceneSwitchScript.instance.scenes[num]);
+                    SceneSwitchScript.instance.aud.clip = SceneSwitchScript.instance.clips[num - 2];
+                    SceneSwitchScript.instance.aud.Play();
+                
+                
             }
         }
         else
